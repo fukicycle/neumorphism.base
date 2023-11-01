dafc3dea73ab3e2ee3bde2ee4620af12191e8e0f
fukicycle <lego.sato.4135@gmail.com>
2023-11-01 01:16:06 -0700

    add auto color selector

---
+++ ShapeType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fukicycle.blazor.neumorphism.components
+{
+    public enum ShapeType
+    {
+        FLOAT,
+        SINK,
+        CONVEX,
+        CONCAVE
+    }
+}
