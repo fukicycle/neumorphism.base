c38ffa46cdad734479c6af0000c01acee32e5c03
fukicycle <lego.sato.4135@gmail.com>
2023-11-01 20:57:17 -0700

    update color settings

---
+++ ColorSetting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fukicycle.Blazor.Neumorphism.Design.Base
+{
+    public static class ColorSetting
+    {
+        public static BaseColor BaseColor { get; set; } = new BaseColor(224, 224, 224);
+    }
+}
